6a5c45e83641b38224082f6a409e2bb70a132257
erkutorenk <52276842+erkutorenk@users.noreply.github.com>
2021-08-28 19:22:36 +0300

    Update send.ts

--- source/utils/send.ts
+++ source/utils/send.ts
@@ -73,9 +73,9 @@ const send = async (
             const userId = subscribe.user_id;
             let text = `<b>${sanitize(feed.feed_title)}</b>`;
             feedItems.forEach(function (item) {
-                text += `\n<a href="${item.link.trim()}">${sanitize(
+                text += `\n${sanitize(
                     item.title
-                )}</a>`;
+                )}`;
             });
             try {
                 await bot.telegram.sendMessage(userId, text, {
